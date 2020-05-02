974f22f757fcd93e8d793d913993bb79b990648d
mithem <mithem@github.com>
2020-05-02 18:13:22 +0200

    hotfix: packaging for PyPi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mithem/PyServer",
-    packages=["serverly"],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
